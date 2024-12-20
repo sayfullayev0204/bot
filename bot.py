0bbcb2ff7d60518043918bb8745eb4d891ed1d51
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-12-20 08:00:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import telebot
 import json
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = '6804578580:AAHJhUTXbOaajQjzURNnT_xXuQQV3smwKC8'
+TOKEN = '7992250612:AAFztka-pZKIa4E9suGNIHSIbkB9tE-Mn-E
 CHANNEL_ID = -1002072810007
-PRIVATE_CHANNEL_LINK = "https://t.me/your_private_channel"
+PRIVATE_CHANNEL_LINK = "https://t.me/+oqCpqlRHAahhODky"
 DATA_FILE = "referral_data.json"
 
 
@@ -60,14 +60,35 @@ def start(message):
         if len(data[user_id]["referrals"]) >= 10:
             bot.send_message(
                 message.chat.id,
-                f"Tabriklaymiz! Siz 10 do‘stni taklif qildingiz! Mana yopiq kanal havolasi:\n{PRIVATE_CHANNEL_LINK}"
+                f"Tabriklaymiz! Siz 10 do‘stni taklif qildingiz! \n\nMana yopiq kanal havolasi:\n\n{PRIVATE_CHANNEL_LINK}"
             )
         else:
-            bot.send_message(
+            with open("img2.jpg", "rb") as photo:
+                bot.send_photo(
                 message.chat.id,
-                f"Assalomu aleykum! Kunkursda g'alaba qozonishingiz uchun botga start bering\n\n"
-                f"{referral_link}\n\n"
+                    photo,
+                    caption=(
+                        "📢 Assalomu alaykum, hurmatli arab tili ixlosmandlari!\n\n"
+                        "Sizni Luqmonjon Muftillayev tomonidan tashkil etilgan “Muammoga yechim” "
+                        "nomli yopiq mentorlik guruhiga qo‘shilishga taklif qilamiz! 🌟\n\n"
+                        "Bu guruh sizning arab tilida duch keladigan har qanday muammolaringiz uchun yechim topadigan joy! 🧠✅\n\n"
+                        "🎯 Siz bu yerda: \n"
+                        "🔹Tilni o‘rganishda yuzaga keladigan savollarga aniq javoblar;\n"
+                        "🔹Foydali maslahatlar va strategiyalar;\n"
+                        "🔹Eksklyuziv bilimlar va tajribalarni olish imkoniyatiga ega bo‘lasiz.\n\n"
+                        "Qo‘shilish shartlari juda oddiy:\n"
+                        "1️⃣ Bizning kanalga obuna bo‘ling.\n"
+                        "2️⃣ 10 nafar do‘stingizni taklif qiling.\n"
+                        "3️⃣ Taklif qilgan 10 nafar do‘stingiz orqali kanalga qo‘shilganingiz tasdiqlangach, "
+                        "sizga yopiq guruhning maxfiy havolasi taqdim etiladi.\n\n"
+                        "🌟 “Muammoga yechim” – arab tili o‘rganishda sizning ishonchli yo‘lboshchingiz bo‘ladi! "
+                        "💡Imkoniyatni qo‘ldan boy bermang! Darhol obuna bo‘ling va o‘z do‘stlaringizni ham qo‘shiling. "
+                        "Birgalikda o‘sish uchun ajoyib imkoniyat!\n\n"
+                        f"📌 Havola ⤵️\n\n{referral_link}"
+                    )
             )
+            
+            
             bot.send_message(
                 message.chat.id,
                 f"Taklif qilingan do'stlaringiz soni: {len(data[user_id]['referrals'])}/10"
@@ -76,16 +97,37 @@ def start(message):
         markup = InlineKeyboardMarkup()
         check_button = InlineKeyboardButton("Aʼzolikni tekshirish", callback_data="check_subscription")
         markup.add(check_button)
+        with open("img1.jpg", "rb") as photo:
+            bot.send_photo(
+                message.chat.id,
+                photo,
+                caption=(
+                    f"Muftillayev Luqmonjon kim?📝\n\n"
+                    f"📌O'zbekiston xalqaro islom akademiyasi 4/4\n"
+                    f"📌Arab tili eksperti\n"
+                    f"📌At Tanaldan <a href=\"https://t.me/c/1673432975/2104\">C2 (muhovara-Nahv-Balog'at)</a>\n"
+                    f"📌Cefrdan C1\n"
+                    f"📌Sinxron Tarjimon\n"
+                    f"📌400 dan ortiq B2-C1 natijali talabalar\n"
+                    f"📌Dublajor\n"
+                    f"📌Mudarris\n"
+                    f"📌10 ta kitob va qo'llanma muallifi\n"
+                    f"📌<a href=\"https://t.me/cefrnatijalar\">Natijalar</a>\n"
+                    f"📌<a href=\"https://t.me/+uVgkeG2kv2dmMjAy\">Bituvchilar fikri</a>\n"
+                    f"📌Qisqacha qilib aytganda arab tili sohasini O'zbekistondagi rivojiga oz bo'lsada hissasini qo'shishga harakat qilayotgan talaba.\n\n"
+                ),
+                parse_mode='HTML'
+                )
 
-        bot.send_message(
-            message.chat.id,
-            f"Salom! Botdan foydalanish uchun ushbu kanalga a'zo bo'ling:\n\n"
-            f"https://t.me/{bot.get_chat(CHANNEL_ID).username}\n\n"
-            "A'zo bo'lganingizdan so'ng, \"Aʼzolikni tekshirish\" tugmasini bosing.",
-            reply_markup=markup
-        )
+            bot.send_message(
+                    message.chat.id,
+                    f"Salom! Botdan foydalanish uchun ushbu kanalga a'zo bo'ling:\n\n"
+                    f"https://t.me/{bot.get_chat(CHANNEL_ID).username}\n\n"
+                    "A'zo bo'lganingizdan so'ng, \"Aʼzolikni tekshirish\" tugmasini bosing.",
+                    reply_markup=markup
+                )
 
-# Callbackni qayta ishlash
+        # Callbackni qayta ishlash
 @bot.callback_query_handler(func=lambda call: call.data == "check_subscription")
 def check_subscription(call):
     user_id = str(call.message.chat.id)
@@ -94,16 +136,35 @@ def check_subscription(call):
     if is_subscribed(call.message.chat.id):
         # Agar a'zo bo'lsa
         referral_link = f"https://t.me/{bot.get_me().username}?start={user_id}"
+        with open("img2.jpg", "rb") as photo:
+                bot.send_photo(
+                call.message.chat.id,
+                    photo,
+                    caption=(
+                        "📢 Assalomu alaykum, hurmatli arab tili ixlosmandlari!\n\n"
+                        "Sizni Luqmonjon Muftillayev tomonidan tashkil etilgan “Muammoga yechim” "
+                        "nomli yopiq mentorlik guruhiga qo‘shilishga taklif qilamiz! 🌟\n\n"
+                        "Bu guruh sizning arab tilida duch keladigan har qanday muammolaringiz uchun yechim topadigan joy! 🧠✅\n\n"
+                        "🎯 Siz bu yerda: \n"
+                        "🔹Tilni o‘rganishda yuzaga keladigan savollarga aniq javoblar;\n"
+                        "🔹Foydali maslahatlar va strategiyalar;\n"
+                        "🔹Eksklyuziv bilimlar va tajribalarni olish imkoniyatiga ega bo‘lasiz.\n\n"
+                        "Qo‘shilish shartlari juda oddiy:\n"
+                        "1️⃣ Bizning kanalga obuna bo‘ling.\n"
+                        "2️⃣ 10 nafar do‘stingizni taklif qiling.\n"
+                        "3️⃣ Taklif qilgan 10 nafar do‘stingiz orqali kanalga qo‘shilganingiz tasdiqlangach, "
+                        "sizga yopiq guruhning maxfiy havolasi taqdim etiladi.\n\n"
+                        "🌟 “Muammoga yechim” – arab tili o‘rganishda sizning ishonchli yo‘lboshchingiz bo‘ladi! "
+                        "💡Imkoniyatni qo‘ldan boy bermang! Darhol obuna bo‘ling va o‘z do‘stlaringizni ham qo‘shiling. "
+                        "Birgalikda o‘sish uchun ajoyib imkoniyat!\n\n"
+                        f"📌 Havola ⤵️\n\n{referral_link}"
+                    )
+            )
+            
         bot.send_message(
-            call.message.chat.id,
-            f"Tabriklaymiz! Siz kanalga aʼzo bo‘lgansiz.\n\n"
-            f"Do‘stlaringizni taklif qilish uchun ushbu xabarni ulashing\n\n"
-        )
-        bot.send_message(
-            call.message.chat.id,
-            f"Assalomu aleykum kunkursda g'alaba qozonishingiz uchun botga start bering\n\n"
-            f"{referral_link}\n\n"
-        )
+                call.message.chat.id,
+                f"Taklif qilingan do'stlaringiz soni: {len(data[user_id]['referrals'])}/10"
+            )
     else:
         bot.send_message(
             call.message.chat.id,
